974dd856bfe8c34e21329e3c8b2fc5cf58ec24e9
Henry Parras <henry.parras@yahoo.com>
2021-05-22 15:18:51 -0700

    getting buttons going

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 var startButton = document.querySelector(".start-button");
 var timerElement = document.querySelector(".timer-count");
-var questionsDisplay = document.querySelector(".questions-display");
 var questionEl = document.querySelector("#questionEl");
 var answerList = document.querySelector("#answerList");
-
-var isWin = false;
 var timer;
 var timerCount;
+var questionCounter = 0;
 
 
 var questions = [{ 
@@ -14,22 +12,44 @@ var questions = [{
         question: "Which one of these is NOT a primitive type",
         choices: ["boolean", "string", "number", "pizza"],
         correctAnswer: "pizza"
-}]
+},
+{
+        question: "In HTML, an img tag if used to display...?",
+        choices: ["links", "videos", "pictures", "footers"],
+        correctAnswer: "pictures"
+},
+{
+        question: "What does CSS apply to your page?",
+        choices: ["storage", "style", "content", "interactivity"],
+        correctAnswer: "style"
+}
+
+];
 
 
 
 function renderFirstQuestion(){
 
-    var questionCounter = 0;
+    
     questionEl.textContent=(questions[questionCounter].question);
 
     
     for (var i = 0; i < questions[questionCounter].choices.length; i++)
     {
-        var liChoice = document.createElement("li");
-        liChoice.textContent = questions[questionCounter].choices[i];
-        answerList.appendChild(liChoice);
+        //var liChoice = document.createElement("li");
+        
+        var answerButton = document.createElement("button");
+        //liChoice.appendChild(liButton)
+        answerButton.textContent = questions[questionCounter].choices[i];
+        answerList.appendChild(answerButton);
+        answerButton.addEventListener("click", checkAnswer);
     }
+}
+
+function checkAnswer(){
+        var correctAnswer = questions[questionCounter].correctAnswer
+        console.log(correctAnswer)
+
 }
 
 // Attach event listener to start button to call startGame function on click
@@ -51,19 +71,18 @@ function startTimer() {
     timer = setInterval(function() {
       timerCount--;
       timerElement.textContent = timerCount;
-      if (timerCount >= 0) {
-        // Tests if win condition is met
-        if (isWin && timerCount > 0) {
-          // Clears interval and stops timer
-          clearInterval(timer);
-          winGame();
-        }
-      }
-      // Tests if time has run out
+      
       if (timerCount === 0) {
         // Clears interval
         clearInterval(timer);
-        loseGame();
+        endGame();
       }
     }, 1000);
+}
+
+  function endGame(){
+
+    questionEl.innerHTML = "Quiz is Over!";
+    answerList.innerHTML =
+      "Better luck next time 👍🏼"
   }
